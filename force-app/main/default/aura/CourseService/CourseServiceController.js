c921bb50fceb549a65eedc5607bef58bdf8c6213
Vladislav Kobrusev <vladislav.kobrusev@vrpconsulting.com>
2019-11-25 10:19:57 +0300

    some changes

--- force-app/main/default/aura/CourseService/CourseServiceController.js
+++ force-app/main/default/aura/CourseService/CourseServiceController.js
@@ -9,11 +9,6 @@
     },
 
     onSubmit: function (component, event, helper) {
-        var firstName = component.get("v.firstName");
-        var lastName = component.get("v.lastName");
-        var email = component.get("v.email");
-        var courseName = component.get("v.courseName");
-        var courseDate = component.get("v.courseDate");
-        helper.handleSubmit(component, firstName, lastName, email, courseName, courseDate);
+        helper.handleSubmit(component);
     }
 })
--- force-app/main/default/aura/CourseService/CourseServiceHelper.js
+++ force-app/main/default/aura/CourseService/CourseServiceHelper.js
@@ -1,11 +1,13 @@
 ({
     handleInit : function(component) {
+        component.set("v.firstDate", null);
+        component.set("v.lastDate", null);
         var action = component.get("c.getCoursesWithDates");
         action.setCallback(this, function(response) {
             var state = response.getState();
 
             if (state === "SUCCESS") {
-                component.set("v.courseDateList", response.getReturnValue().courseDateList);
+                component.set("v.courseDateMap", response.getReturnValue().courseDateMap);
                 var courseList = [];
                 for (var i = 0; i < response.getReturnValue().courseNameList.length; i++) {
                     courseList.push({
@@ -35,9 +37,24 @@
 
     handleChange: function (component, selectedOptionValue) {
         component.set("v.courseName", selectedOptionValue);
+        var courseDateMap = component.get("v.courseDateMap");
+        var selectedCourseDate = courseDateMap[selectedOptionValue];
+
+        if (selectedCourseDate) {
+            component.set("v.firstDate", selectedCourseDate[0]);
+            component.set("v.lastDate", selectedCourseDate[courseDateMap[selectedOptionValue].length - 1]);
+            component.set("v.isUnassigned", false);
+        } else {
+            component.set("v.isUnassigned", true);
+        }
     },
 
-    handleSubmit: function (component, firstName, lastName, email, courseName, courseDate) {
+    handleSubmit: function (component) {
+        var firstName = component.get("v.firstName");
+        var lastName = component.get("v.lastName");
+        var email = component.get("v.email");
+        var courseName = component.get("v.courseName");
+        var courseDate = component.get("v.courseDate");
         var action = component.get("c.registerCourseContact");
         action.setParams({
             firstName: firstName,
@@ -50,7 +67,7 @@
             var state = response.getState();
 
             if (state === "SUCCESS") {
-                var msgSuccess = response.getReturnValue();
+                var msgSuccess = "You successfully registered";
                 this.handleShowToast(component, state, msgSuccess);
             } else if (state === "ERROR") {
                 var errors = response.getError();
